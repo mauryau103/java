b43f41c43af6f62eae9ea9707f3d798e935b3c1b
Umesh Maurya <mauryau103@gmail.com>
2024-03-06 12:38:54 +0000

    added some initial notes about first program

--- 01_Basics/FirstProgram.java
+++ 01_Basics/FirstProgram.java
@@ -7,3 +7,11 @@ public class FirstProgram {
         System.out.println("Hello world");
     }
 }
+
+//class keyword is used to declare a class in java
+//public keyword is a access modifier which represent visibility.It means it is visible to all
+//static is a keyword.If we declare any method as a static, it is known as a static method.the core advantage of the static method is that there is no need to create an object to invoke the static method.The main method is executed by JVM,so it does'nt require to create an object to invoke the method.so it saves memory.
+//void is the return type of the method.it means it does not return any value.
+//main return the starting point of the program
+//String[] args is used for command line argument.
+//System.out.println() -->it is used print statement.
