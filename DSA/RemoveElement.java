3442d019893d4d911a4809d90827ecfd77e91f10
Umesh Maurya <mauryau103@gmail.com>
2024-03-23 07:29:41 +0000

    remove element is done

--- DSA/RemoveElement.java
+++ DSA/RemoveElement.java
@@ -1,5 +1,24 @@
 package DSA;
 
+import java.util.Arrays;
+
 public class RemoveElement {
-    
+    public static void main(String[] args) {
+
+        int[] nums = { 3, 2, 2, 3 };
+        int val = 3;
+
+        System.out.println(removeElement(nums, val)); // output-> 2
+    }
+
+    public static int removeElement(int[] nums, int val) {
+        int index = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != val) {
+                nums[index++] = nums[i];
+            }
+            System.out.println(Arrays.toString(nums));
+        }
+        return index;
+    }
 }
