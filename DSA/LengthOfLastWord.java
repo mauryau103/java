d981992fd8f49e8bcc4ee3c94f0d3cd25851fabd
Umesh Maurya <mauryau103@gmail.com>
2024-03-23 18:16:43 +0000

    Length of last word is begin

---
+++ DSA/LengthOfLastWord.java
@@ -0,0 +1,20 @@
+package DSA;
+
+/**
+ * LengthOfLastWord
+ * https://leetcode.com/problems/length-of-last-word/description/
+ */
+public class LengthOfLastWord {
+
+    public static void main(String[] args) {
+        String s = "Hello World";
+        // "   fly me   to   the moon  "  -->4
+        //"luffy is still joyboy"  -->6
+        //"Hello World"  -->5
+        System.out.println(lengthOfLastWord(s));
+    }
+
+    public static int lengthOfLastWord(String s) {
+        return 0;
+    }
+}
