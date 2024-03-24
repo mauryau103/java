35ff0f02375dc942bc1367ab7bf8e2eb24096cd8
Umesh Maurya <mauryau103@gmail.com>
2024-03-24 15:32:52 +0000

    Length of last word is done

--- DSA/LengthOfLastWord.java
+++ DSA/LengthOfLastWord.java
@@ -7,7 +7,7 @@ package DSA;
 public class LengthOfLastWord {
 
     public static void main(String[] args) {
-        String s = "Hello World";
+        String s = "Hello World     ";
         // "   fly me   to   the moon  "  -->4
         //"luffy is still joyboy"  -->6
         //"Hello World"  -->5
@@ -15,6 +15,8 @@ public class LengthOfLastWord {
     }
 
     public static int lengthOfLastWord(String s) {
-        return 0;
+        s = s.trim();
+        String[] stringArray = s.split(" ");
+        return stringArray[stringArray.length-1].length();
     }
 }
