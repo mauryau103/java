c3df913e9a55abbbfbf71eaf3664f545d13c5d82
Umesh M <umesh.maurya@neosoftmail.com>
2024-03-29 13:38:36 +0530

    odd cells started

---
+++ DSA/CellsWithOddValuesInMatrix.java
@@ -0,0 +1,18 @@
+package DSA;
+
+public class CellsWithOddValuesInMatrix {
+    public static void main(String[] args) {
+        int m = 2;
+        int n = 3;
+        int[][] indices = {{0,1},{1,1}};
+        System.out.println(oddCells(m, n, indices));
+    }
+
+    public static int oddCells(int m, int n, int[][] indices) {
+        int[][] arr = new int[m][n];
+            for(int i =0;i<arr.length;i++){
+            
+            }
+        return 0;
+    }
+}
